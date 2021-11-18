9ab7b21994bae746ad8948e0eccb677b4919952d
asphixia <60938767+asphixia@users.noreply.github.com>
2021-11-18 23:06:19 +0100

    Add files via upload

---
+++ wqe.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.company.Main;
+
+import java.util.ArrayList;
+
+public class wqe extends Main {
+    private ArrayList<String> teams;
+
+
+
+
+    public void readTeams(){
+
+        Main something = new Main();
+        Main.getTeams().add("hi");
+
+    }
+
+
+
+}
+
+
